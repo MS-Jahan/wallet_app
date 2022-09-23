9ebec8b8c9526832f2560cd5aa4bddae670c3c9e
ms-jahan <ssarwarjahan@gmail.com>
2022-09-23 11:46:44 +0600

    Removed tabs and added two chips

--- lib/Screens/Dashboard/dashboard.dart
+++ lib/Screens/Dashboard/dashboard.dart
@@ -15,63 +15,129 @@ class Dashboard extends StatefulWidget {
 class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2, // Added
-      initialIndex: 0,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-          backgroundColor: const Color(0xFF01AA71),
-          // elevation: 0,
-          bottom: const TabBar(
-            indicatorColor: Colors.white,
-            tabs: [
-              Tab(
-                child: Text(
-                  "Accounts",
-                  overflow: TextOverflow.clip,
-                  maxLines: 1,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: const Color(0xFF01AA71),
+      ),
+      drawer: DrawerWidget(),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "List of Accounts",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              Tab(
-                child: Text(
-                  "Records",
-                  overflow: TextOverflow.clip,
-                  maxLines: 1,
-                ),
-              ),
-            ],
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Icon(Icons.settings),
+                  style: ButtonStyle(
+                    shadowColor: MaterialStateProperty.all<Color>(Colors.white),
+                    elevation: MaterialStateProperty.all<double>(0),
+                    padding: MaterialStateProperty.all<EdgeInsets>(
+                      EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                    ),
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                      Colors.white,
+                    ),
+                    foregroundColor: MaterialStateProperty.all<Color>(
+                      Colors.blue,
+                    ),
+                    side: MaterialStateProperty.all<BorderSide>(
+                        BorderSide(color: Color.fromARGB(255, 214, 214, 214))),
+                    tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    minimumSize: MaterialStateProperty.all<Size>(
+                      Size(0, 0),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
-        ),
-        drawer: DrawerWidget(),
-        body: ColoredBox(
-          color: Color(0xFF01AA71),
-          // child: TabBar(
-          //   // labelColor: Colors.black,
-          //   indicatorColor: Colors.white,
-          //   tabs: [
-          // Tab(
-          //   child: Text(
-          //     "Tab 1",
-          //     overflow: TextOverflow.clip,
-          //     maxLines: 1,
-          //   ),
-          // ),
-          // Tab(
-          //   child: Text(
-          //     "Tab 2",
-          //     overflow: TextOverflow.clip,
-          //     maxLines: 1,
-          //   ),
-          // ),
-          //   ],
-          // ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          tooltip: 'Increment',
-          child: const Icon(Icons.add),
-        ),
+          Container(
+            margin: EdgeInsets.all(10),
+            child: Row(
+              //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    margin: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 3),
+                    child: Container(
+                      padding: EdgeInsets.symmetric(vertical: 3, horizontal: 5),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Cash",
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 236, 236, 236),
+                            ),
+                          ),
+                          Text(
+                            "BDT 7,300.00",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                        // color: Colors.blue,
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        // shape: BoxShape.rectangle,
+                        border: Border.all(
+                            color: Color.fromARGB(255, 62, 106, 249))),
+                    margin: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 3),
+                    child: Container(
+                      padding: EdgeInsets.symmetric(vertical: 0, horizontal: 5),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Add Account".toUpperCase(),
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 62, 106, 249),
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(Icons.add_circle),
+                            color: Color.fromARGB(255, 62, 106, 249),
+                            padding: EdgeInsets.all(0),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
       ),
     );
   }
